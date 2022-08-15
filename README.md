6d4a1893e0d18ad13565cffa0a23d43bdf138644
sohamkamani <sohamkamani@gmail.com>
2022-08-15 17:23:40 +0530

    working example

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# {TITLE}
+# Kafka Producer/Consumer Example in Java
 
-This is the example repo for my blog post on [Running a Java Application on the Command Line With Maven](https://sohamkamani.com/java/cli-app-with-maven/)
+This is the example repo for my blog post on [Running a Kafka Producer/Consumer Example in Java](https://sohamkamani.com/java/kafka/)
 
 To run this example:
 
 1. Make sure you have Java, the JDK, and Maven installed
 1. Clone the repo
 2. Run `mvn clean compile assembly:single` - this should give you a JAR file in the `target` folder
-3. Run `java -jar target/{PROJECTDIR}-1.0-SNAPSHOT-jar-with-dependencies.jar` to run the JAR file
+3. Run `java -jar target/java-kafka-example-1.0-SNAPSHOT-jar-with-dependencies.jar` to run the JAR file
--- src/main/java/com/sohamkamani/App.java
+++ src/main/java/com/sohamkamani/App.java
@@ -1,20 +1,108 @@
 package com.sohamkamani;
 
-import com.github.ricksbrown.cowsay.Cowsay;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main(String[] args) {
-        int result = Calculator.add(4, 5);
-        String[] cowArgs = new String[] { String.valueOf(result) };
-        String cowString = Cowsay.say(cowArgs);
-        System.out.printf(cowString);
+
+    private static final String TOPIC = "my-kafka-topic";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9093,localhost:9094,localhost:9095";
+
+    //
+    public static void main(String[] args) throws Exception {
+        Thread consumerThread = new Thread(App::consume);
+        consumerThread.start();
+
+        Thread producerThread = new Thread(App::produce);
+        producerThread.start();
     }
 
-    public static int add(int n1, int n2) {
-        return n1 + n2;
+    private static void produce() {
+        // Create configuration options for our producer
+        Properties props = new Properties();
+        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        // We configure the serializer to describe the format in which we want to produce data into
+        // our Kafka cluster
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // wait until we get 10 messages before writing
+        props.put("batch.size", "10");
+        // no matter what happens, write all pending messages
+        // every 2 seconds
+        props.put("linger.ms", "2000");
+        props.put("acks", "1");
+
+        // Since we need to close our producer, we can use the try-with-resources statement to
+        // create a new producer
+        try (Producer<String, String> producer = new KafkaProducer<>(props)) {
+            // here, we run an infinite loop to sent a message to the cluster every second
+            for (int i = 0;; i++) {
+                String key = Integer.toString(i);
+                String message = "this is message " + Integer.toString(i);
+
+                producer.send(new ProducerRecord<String, String>(TOPIC, key, message));
+
+                // log a confirmation once the message is written
+                System.out.println("sent msg " + key);
+                try {
+                    // Sleep for a second
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Could not start producer: " + e);
+        }
     }
+
+    private static void consume() {
+        // Create configuration options for our consumer
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", BOOTSTRAP_SERVERS);
+        // The group ID is a unique identified for each consumer group
+        props.setProperty("group.id", "my-group-id4");
+        // Since our producer uses a string serializer, we need to use the corresponding
+        // deserializer
+        props.setProperty("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        // Every time we consume a message from kafka, we need to "commit" - that is, acknowledge
+        // receipts of the messages. We can set up an auto-commit at regular intervals, so that
+        // this is taken care of in the background
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("fetch.min.bytes", "150");
+        props.setProperty("fetch.max.wait.ms", "2500");
+        props.setProperty("auto.offset.reset", "earliest");
+
+        // Since we need to close our consumer, we can use the try-with-resources statement to
+        // create it
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            // Subscribe this consumer to the same topic that we wrote messages to earlier
+            consumer.subscribe(Arrays.asList(TOPIC));
+            // run an infinite loop where we consume and print new messages to the topic
+            while (true) {
+                // The consumer.poll method checks and waits for any new messages to arrive for the
+                // subscribed topic
+                // in case there are no messages for the duration specified in the argument (1000 ms
+                // in this case), it returns an empty list
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("received message: %s\n", record.value());
+                }
+            }
+        }
+    }
+
 }
+
